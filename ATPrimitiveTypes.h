2e698e6e02df3e952d7b4b36d8505242889431e7
Rafael Jaimes III <rafjaimes@gmail.com>
2015-08-13 15:21:48 -0400

    Create ATPrimitiveTypes.h

---
+++ ATPrimitiveTypes.h
@@ -0,0 +1,20 @@
+#ifndef ATPRIMITIVETYPES_H
+#define ATPRIMITIVETYPES_H
+
+typedef char                    AT_8;
+typedef unsigned char           AT_U8;
+
+typedef short                   AT_16;
+typedef unsigned short          AT_U16;
+
+typedef char                    AT_C;
+
+
+typedef int                 AT_32;
+typedef unsigned int        AT_U32;
+
+ 
+typedef long long             AT_64;
+typedef long unsigned long    AT_U64;
+ 
+#endif
